c3e9fe24f838df0cd3abdb58d55c952a3356b4c7
wechat <2102980693@qq.com>
2020-06-04 15:59:51 +0800

    we1

---
+++ config/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Peanut
+ * @Description:  全局配置config
+ * @Date: 2020-05-20 22:36:09
+ * @Last Modified by: Peanut
+ * @Last Modified time: 2020-05-24 00:11:23
+ */
+module.exports = {
+  TOKEN: "puppet_padplus_170a1c278eb924fc",
+  IGNORE: ["hell"], //忽略某个用户的消息，填写用户昵称即可
+  WEBROOM: "王者峡谷里的小猪猪", //要管理的群名称
+  MYSELF: "马" //大号的备注！！！
+};
---
+++ node_modules/_@chatie_eslint-config@0.6.20@@chatie/eslint-config/scripts/npm-pack-testing.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+npm run dist
+npm run pack
+
+TMPDIR="/tmp/npm-pack-testing.$$"
+mkdir "$TMPDIR"
+mv *-*.*.*.tgz "$TMPDIR"
+cp -R tests/fixtures/* "$TMPDIR"
+
+cd $TMPDIR
+npm init -y
+npm install --production \
+  *-*.*.*.tgz \
+  @types/blue-tape \
+  @types/eslint \
+  @types/glob \
+  @types/node \
+  @chatie/tsconfig \
+  blue-tape \
+  glob \
+  typescript
+
+./node_modules/.bin/tsc \
+  --lib esnext \
+  --strict \
+  --esModuleInterop \
+  --noEmitOnError \
+  --noImplicitAny \
+  --skipLibCheck \
+  smoke-testing.ts
+
+mv eslintrc.yaml .eslintrc.yaml
+
+node smoke-testing.js
